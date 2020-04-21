4def8c1f79b682e18c3bc7ccb2fc55561fd9fd05
CaioFauza <caiofauza@gmail.com>
2020-04-20 22:06:17 -0300

    Updates

---
+++ precache-manifest.998cce21a7e27c12a9fa62cc27cbcc31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e954a4b3d3399f06af6cfd3d8c2501fe",
+    "url": "/qt-landing/index.html"
+  },
+  {
+    "revision": "641c6b644fa5099d4756",
+    "url": "/qt-landing/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b459c749e583b8f05511",
+    "url": "/qt-landing/static/js/2.fc709fe8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/qt-landing/static/js/2.fc709fe8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "641c6b644fa5099d4756",
+    "url": "/qt-landing/static/js/main.c754e9e1.chunk.js"
+  },
+  {
+    "revision": "97217beb4f84c2c0d43f",
+    "url": "/qt-landing/static/js/runtime-main.446d8146.js"
+  },
+  {
+    "revision": "0ab059c741cacb08eef7d4d54c84b121",
+    "url": "/qt-landing/static/media/qt_cor-01.0ab059c7.png"
+  }
+]);
